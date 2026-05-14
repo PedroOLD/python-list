906ed408ac692200a62e90c64f1a77e5358cd12f
Pedro Old <pedro_henrique_santos_corebiz@whirlpool.com>
2026-05-14 18:03:34 -0300

    feat(py02): ex0

--- pythonModule02/ex0/ft_first_exception.py
+++ pythonModule02/ex0/ft_first_exception.py
@@ -1,2 +1,39 @@
-def ft_first_exception(temp_str):
-    print("aqui")
+def input_temperature(temp_str):
+    """
+        converte uma string em um number
+    """
+    number: int = int(temp_str)
+    return number
+
+def test_temperature():
+    """
+        test as funcoes de input
+    """
+    print("=== Garden Temperature ===")
+    
+    print()
+    print('Input data is "25"')
+    try:
+        current_number = input_temperature("25")
+        print(f"Temperature is now {current_number}°C")
+    except Exception as e:
+        print("Caught input_temperature error", e)
+    
+    print()
+    print('Input data is "25b"')
+    try:
+        current_number = input_temperature("25b")
+        print(f"Temperature is now {current_number}°C")
+    except Exception as e:
+        print("Caught input_temperature error", e)
+    
+    print()
+    print('Input data is "abc"')
+    try:
+        current_number = input_temperature("abc")
+        print(f"Temperature is now {current_number}°C")
+    except Exception as e:
+        print("Caught input_temperature error", e)
+
+if __name__ == "__main__":
+    test_temperature()
